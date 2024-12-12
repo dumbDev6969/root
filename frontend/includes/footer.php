d297ee572ccb18dabc796d2b7f34c6e2aebcdf31
Joshua Cabuang <joshuacabuang0@gmail.com>
2024-12-12 09:16:56 +0300

    footer first ver

---
+++ frontend/includes/footer.php
@@ -0,0 +1,53 @@
+
+    <style>
+        .nav-f, .nav-link-f{
+            color: #6C757D;
+        }
+        .row{
+            width: 100%
+        }
+        footer{
+            width: 100%
+        }
+    </style>
+
+    <footer>
+        <div class="row d-flex  align-items-center bg-dark">
+            <div class="col-md-3">
+                <div class="container text-secondary">
+                    <h1>Search</h1>
+                    <h5>copyright 2024</h5>
+                    <p>search seach</p>
+                </div>
+            </div>
+            <div class="col-md-9 ">
+                <div class="container d-flex  align-items-center justify-content-evenly p-3">
+                    <div class="section d-flex flex-column">
+                        <a class="nav-f" href="">Home</a>
+                        <a class="nav-f" href="">About</a>
+                    </div>
+                    <div class="section d-flex flex-column">
+                        <a  class="nav-f"href="">Our mission</a>
+                        <a class="nav-f" href="">About</a>
+                    </div>
+                    <button type="button" class="btn btn-secondary">Contact Us</button>
+                </div>
+                <hr class="text-light">
+               
+                <div class=" d-flex justify-content-end ">
+                    <ul class="nav d-flex  justify-content-between" style="width: 150px ">
+                        <li class="nav-item">
+                            <a class="nav-link-f" aria-current="page" href="#"><i class="bi bi-facebook"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link-f " href="#"><i class="bi bi-twitter-x"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link-f" href="#"><i class="bi bi-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+          
+            </div>
+        </div>
+    </footer>
