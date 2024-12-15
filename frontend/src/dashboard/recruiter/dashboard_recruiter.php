70b11399fa311137d3c6e495ecd70c6a18bd9513
Joshua Cabuang <joshuacabuang0@gmail.com>
2024-12-15 14:28:24 +0530

    change teh width of columns

--- frontend/src/dashboard/recruiter/dashboard_recruiter.php
+++ frontend/src/dashboard/recruiter/dashboard_recruiter.php
@@ -9,6 +9,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="../../../assets/links.css" rel="stylesheet" />
+    <style>
+        .col-lg-3{
+            width: 15rem
+        }
+    </style>
 </head>
 
 <body>
@@ -102,13 +107,44 @@
                         </div>
                     </div>
                     <h4 id="simple-list-item-2">Jobs</h4>
-                    <div class="row gap-2">
-                        <div class="col-lg-3 col-md-12">
-                            <div class="card" style="width: 18rem;">
+                    <div class="row gap-3">
+                        <div class="col-lg-3 col-md-12 " >
+                            <div class="card" style="width: 15.5rem;">
                                 <div class="card-body">
                                     <div class="d-flex  justify-content-between align-items-center">
                                         <div>
-                                            <h4 class="card-title">Jr. Sofware dev</h4>
+                                            <h5 class="card-title">Jr. Sofware dev</h5>
+                                            
+                                        </div>
+                                        <div class="company-profile border" style="height: 50px; width: 50px; border-radius: 50px">
+                                            img
+                                        </div>
+                                    </div>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">Company name</h6>
+                                    <p class="text-secondary">Pangasinan SCCP</p>
+                                    <p class="card-text">
+                                        <button type="button" disabled class="btn btn-outline-secondary btn-sm">Part time</button>
+                                        <h6>$100 - $120</h6>
+                                    </p>
+                                    <form action="#">
+                                        <div class="row">
+                                            <div class="col-md-8 p-1">
+                                                <button type="button" class="btn btn-sm btn-dark w-100">More details</button>
+                                            </div>
+                                            <div class="col-md-4 p-1">
+                                                <button type="button" class="btn btn-sm btn-secondary w-100">Save</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12">
+                            <div class="card" style="width: 15.5rem;">
+                                <div class="card-body">
+                                    <div class="d-flex  justify-content-between align-items-center">
+                                        <div>
+                                            <h5 class="card-title">Jr. Sofware dev</h5>
                                             
                                         </div>
                                         <div class="company-profile border" style="height: 50px; width: 50px; border-radius: 50px">
